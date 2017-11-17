99e8cfab87df45eed8a8c5f2b714fbb60d438bdf
Will Russell <Will@MacBook-Pro-2.local>
2017-11-17 16:23:26 -0500

    remove the thing

--- app/main/main_ctrl.js
+++ app/main/main_ctrl.js
@@ -2,21 +2,10 @@
 
   'use strict';
 
-  starterApp.controller('main_ctrl', function(ExampleFactory) {
+  starterApp.controller('main_ctrl', function() {
 
     var vm = this; // view model 
 
-    ExampleFactory.query(function(data) {
-
-      var roster = JSON.parse(angular.toJson(data));
-
-      var activeFighters = _.filter(roster, { "fighter_status": "Active" });
-
-      console.log(activeFighters.length);
-
-      vm.actives = activeFighters;
-
-    });
 
   });
 })(window.angular, window.starterApp);

d35722fd3aae582d9075fbae0ebee8ee0e150c69
Will Russell <Will@MacBook-Pro-3.local>
2017-12-14 16:17:23 -0500

    list the dependencies

--- app/about/about_ctrl.js
+++ app/about/about_ctrl.js
@@ -6,6 +6,19 @@
 
     var vm = this; // view model 
 
+    vm.components = [
+      'Angular (1.6.6)',
+      'Angular-Material (1.1.5)',
+      'ngAnimate',
+      'ngCookies',
+      'ngResource',
+      'UI-Router',
+      'Moment.js',
+      'Underscore.js',
+      'Material Icons',
+      'Roboto web font'
+    ];
+
 
   });
 })(window.angular, window.starterApp);
